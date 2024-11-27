fd97a3e0da67ec6d0afb4a6613e2c8a11408de8d
Loveria Diskless PH <Loveria Diskless PH@VIP06>
2024-11-27 19:24:43 +0800

    Add project files.

---
+++ Samapage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Samapage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
